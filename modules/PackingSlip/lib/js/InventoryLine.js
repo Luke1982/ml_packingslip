c398ad413d9ad554f5491cdca455294febbd656f
Luke1982 <gcx3@hotmail.com>
2016-10-20 17:03:59 +0200

    Build InventoryLine JS Class

--- modules/PackingSlip/lib/js/InventoryLine.js
+++ modules/PackingSlip/lib/js/InventoryLine.js
@@ -1,7 +1,20 @@
 function InventoryLine(data) {
-	this.props = __getInventoryLineProps();
+	this.propInputs = __getInventoryLineProps();
+	this.props = {};
 
 	function __getInventoryLineProps() {
-		return document.getElementsByClassName("productline_props")[data.index];
+		return document.getElementsByClassName("productline_props")[data.index].getElementsByTagName("input");
+	}
+
+	this.__setLineProperty = function(propertyName, propertyValue) {
+		this.props[propertyName] = propertyValue;
+	}
+
+	this.setProps = function() {
+		for (var i = 0; i < this.propInputs.length; i++) {
+			var propName = this.propInputs[i].className.replace("hdn_product_", "");
+			var propVal = this.propInputs[i].value;
+			this.__setLineProperty(propName, propVal);
+		}
 	}
 }
