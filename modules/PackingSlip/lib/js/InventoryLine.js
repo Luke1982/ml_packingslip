b124b733905fd5c043166d34300075afd805e37e
Luke1982 <gcx3@hotmail.com>
2016-11-03 15:33:03 +0100

    Prepare values for inputs that are numeric. Also use this function on autocomplete fields

--- modules/PackingSlip/lib/js/InventoryLine.js
+++ modules/PackingSlip/lib/js/InventoryLine.js
@@ -82,7 +82,12 @@ function InventoryLine(data) {
 	}
 
 	__setInput = function(input, newValue) {
-		console.log(input);
+		// If input is numeric type remove dots
+		// and replace comma by dot
+		if (input.type == "number") {
+			newValue = newValue.replace(".", "");
+			newValue = newValue.replace(",", ".");
+		}
 		input.value = newValue;
 		return newValue;
 	}
@@ -439,9 +444,11 @@ function InventoryLine(data) {
 	function __autocompleteCallback(label, callingLine, source) {
 		for(var i = 0; i < source.length; i++) {
 			if (source[i].label == label) {
+				// Numeric inputs
+				__setInput(callingLine.getElementsByClassName("product_line_listprice")[0], source[i].price);
+				__setInput(callingLine.getElementsByClassName("product_line_costprice")[0], source[i].costPrice);
+				// Other inputs
 				callingLine.getElementsByClassName("product_line_comment")[0].value = source[i].desc;
-				callingLine.getElementsByClassName("product_line_listprice")[0].value = source[i].price;
-				callingLine.getElementsByClassName("product_line_costprice")[0].value = source[i].costPrice;
 				callingLine.getElementsByClassName("hdn_product_crm_id")[0].value = source[i].crmid;
 				callingLine.getElementsByClassName("hdn_product_entity_type")[0].value = source[i].entityType;
 				// Fill in the target spans
