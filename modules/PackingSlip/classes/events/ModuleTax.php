9aec97131ca6822f5caa856550eb2fc757544519
Luke1982 <gcx3@hotmail.com>
2016-11-30 22:53:08 +0100

    Normalize the column names for SH taxes

--- modules/PackingSlip/classes/events/ModuleTax.php
+++ modules/PackingSlip/classes/events/ModuleTax.php
@@ -7,7 +7,7 @@ class ModuleTax extends VTEventHandler{
 	public function handleEvent($name, $data){
 		global $adb;
 
-		$name = $data['tax_type'] == 'sh' ? 'shtax'.$data['tax_id'] : 'grouptax_'.$data['tax_id'];
+		$name = $data['tax_type'] == 'sh' ? 'shtax_'.$data['tax_id'] : 'grouptax_'.$data['tax_id'];
 		$percentage = $data['tax_value'];
 
 		$q = "ALTER TABLE $this->table_name ADD COLUMN $name DECIMAL(7,3) DEFAULT $percentage";
